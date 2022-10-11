55f4929f1b5a6708e81556e2852591d60fe3bed1
Johnscor <90119848+Johnscor@users.noreply.github.com>
2022-10-10 21:34:02 -0300

    Alexa radio App SKILL
    Radio Skill for amazon ALEXA

---
+++ lambda/index.js
@@ -0,0 +1,193 @@
+/* eslint-disable no-mixed-operators */
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+
+const STREAMS = [
+  {
+    'token': 'dabble-radio-1',
+    'url': 'https://alternativa104fm.page.link/Radio104fm38uv',
+    'metadata': {
+      'title': 'Alternativa 104,9 FM',
+      'subtitle': 'A rádio que mais toca você!',
+      'art': {
+        'sources': [
+          {
+            'contentDescription': 'Dabble Radio',
+            'url': 'https://live.staticflickr.com/65535/52046580668_18aedc8ddd.jpg',
+            'widthPixels': 512,
+            'heightPixels': 512,
+          },
+        ],
+      },
+      'backgroundImage': {
+        'sources': [
+          {
+            'contentDescription': 'Alternativa Cento e Quatro vírgula Nove FM',
+            'url': 'https://live.staticflickr.com/65535/52047648877_55fa6c28aa_b.jpg',
+            'widthPixels': 1200,
+            'heightPixels': 800,
+          },
+        ],
+      },
+    },
+  },
+];
+
+const PlayStreamIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
+      || handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (
+        handlerInput.requestEnvelope.request.intent.name === 'PlayStreamIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.ResumeIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.LoopOnIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.PreviousIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.RepeatIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.ShuffleOnIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StartOverIntent'
+      );
+  },
+  handle(handlerInput) {
+    const stream = STREAMS[0];
+
+    handlerInput.responseBuilder
+      .speak(`iniciando ${stream.metadata.title}`)
+      .addAudioPlayerPlayDirective('REPLACE_ALL', stream.url, stream.token, 0, null, stream.metadata);
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'Esta habilidade reproduz um fluxo de áudio quando é iniciada. Não possui nenhuma funcionalidade adicional';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const AboutIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AboutIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'Esta é uma habilidade de streaming de áudio para rádio criada por john';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (
+        handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.PauseIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.LoopOffIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.ShuffleOffIntent'
+      );
+  },
+  handle(handlerInput) {
+    handlerInput.responseBuilder
+      .addAudioPlayerClearQueueDirective('CLEAR_ALL')
+      .addAudioPlayerStopDirective();
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const PlaybackStoppedIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'PlaybackController.PauseCommandIssued'
+      || handlerInput.requestEnvelope.request.type === 'AudioPlayer.PlaybackStopped';
+  },
+  handle(handlerInput) {
+    handlerInput.responseBuilder
+      .addAudioPlayerClearQueueDirective('CLEAR_ALL')
+      .addAudioPlayerStopDirective();
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const PlaybackStartedIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'AudioPlayer.PlaybackStarted';
+  },
+  handle(handlerInput) {
+    handlerInput.responseBuilder
+      .addAudioPlayerClearQueueDirective('CLEAR_ENQUEUED');
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const ExceptionEncounteredRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'System.ExceptionEncountered';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return true;
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    console.log(handlerInput.requestEnvelope.request.type);
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    PlayStreamIntentHandler,
+    PlaybackStartedIntentHandler,
+    CancelAndStopIntentHandler,
+    PlaybackStoppedIntentHandler,
+    AboutIntentHandler,
+    HelpIntentHandler,
+    ExceptionEncounteredRequestHandler,
+    SessionEndedRequestHandler,
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
